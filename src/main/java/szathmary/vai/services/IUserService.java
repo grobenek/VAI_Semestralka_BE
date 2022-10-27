4fcbd4de39a2be17df99b06ad516128c136daed3
Peter Szathmary <peter.szathmary01@gmail.com>
2022-10-27 11:15:01 +0200

    User service layer created

---
+++ src/main/java/szathmary/vai/services/IUserService.java
@@ -0,0 +1,15 @@
+package szathmary.vai.services;
+
+import szathmary.vai.entities.User;
+
+import java.util.List;
+
+public interface IUserService {
+    List<User> getAllUsers();
+
+    User getUserById(Integer id);
+
+    User updateUser(User userToUpdate);
+
+    void deleteUser(User userToDelete);
+}
---
+++ src/main/java/szathmary/vai/services/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package szathmary.vai.services;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import szathmary.vai.entities.User;
+import szathmary.vai.repositories.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements IUserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
+    }
+
+    @Override
+    public User getUserById(Integer id) {
+        return this.userRepository.getReferenceById(id);
+    }
+
+    @Override
+    public User updateUser(User userToUpdate) {
+        User existingUser = this.getUserById(userToUpdate.getUserId());
+
+        BeanUtils.copyProperties(existingUser, userToUpdate, "userId");
+
+        return this.userRepository.save(userToUpdate);
+    }
+
+    @Override
+    public void deleteUser(User userToDelete) {
+        this.userRepository.delete(userToDelete);
+    }
+}
