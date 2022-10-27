56097fcf6fb5c4d36100d9c320fd0a7e4c476554
Peter Szathmary <peter.szathmary01@gmail.com>
2022-10-27 11:11:37 +0200

    connecting to database

---
+++ src/main/java/szathmary/vai/VaiApiApplication.java
@@ -0,0 +1,13 @@
+package szathmary.vai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/szathmary/vai/VaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package szathmary.vai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
