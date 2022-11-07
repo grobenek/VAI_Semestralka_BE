5cba0e52901b3d32b583f367e9bb6cf57a3c97c8
Peter Szathmary <peter.szathmary01@gmail.com>
2022-11-07 11:26:41 +0100

    CategoryNameRepository created

---
+++ src/main/java/szathmary/vai/repositories/CategoryNameRepository.java
@@ -0,0 +1,8 @@
+package szathmary.vai.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import szathmary.vai.entities.CategoryName;
+
+public interface CategoryNameRepository extends JpaRepository<CategoryName, Integer> {
+
+}
