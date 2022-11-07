77eb64e9a0995223e234b41cdd2fbd37cf2ef631
Peter Szathmary <peter.szathmary01@gmail.com>
2022-11-07 11:48:10 +0100

    CategoryNameServiceImpl created

---
+++ src/main/java/szathmary/vai/services/CategoryNameServiceImpl.java
@@ -0,0 +1,43 @@
+package szathmary.vai.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import szathmary.vai.entities.CategoryName;
+import szathmary.vai.repositories.CategoryNameRepository;
+
+@Service
+public class CategoryNameServiceImpl implements ICategoryNameService {
+
+  CategoryNameRepository categoryNameRepository;
+
+  @Autowired
+  public CategoryNameServiceImpl(CategoryNameRepository categoryNameRepository) {
+    this.categoryNameRepository = categoryNameRepository;
+  }
+
+  @Override
+  public List<CategoryName> getAllCategoryNames() {
+    return this.categoryNameRepository.findAll();
+  }
+
+  @Override
+  public CategoryName getCategoryNameById(Integer id) {
+    return this.categoryNameRepository.getReferenceById(id);
+  }
+
+  @Override
+  public void updateCategoryName(CategoryName categoryNameToUpdate) {
+    this.categoryNameRepository.save(categoryNameToUpdate);
+  }
+
+  @Override
+  public void deleteCategoryName(CategoryName categoryNameToDelete) {
+    this.categoryNameRepository.deleteById(categoryNameToDelete.getCategoryNameId());
+  }
+
+  @Override
+  public CategoryName createCategoryName(CategoryName categoryNameToCreate) {
+    return this.categoryNameRepository.save(categoryNameToCreate);
+  }
+}
